012a24d580a70cd30339d691ca235e7dfce8c1a5
thanhhoan <pdth456@gmail.com>
2021-08-16 22:44:37 +0700

    testing img load speed

--- components/UI/Featured.js
+++ components/UI/Featured.js
@@ -21,50 +21,23 @@ export default function Featured() {
           {megaPokemons.data.slice(0, 40).map((e) => (
             <div key={e.id} className="card">
               <section className="img-container">
-                <div className="tilt-inner">
-                  {e ? (
-                    <Image src={e.images.large} layout="fill" objectFit="fit" />
-                  ) : (
-                    <ContentLoader
-                      viewBox="0 0 500 280"
-                      height={300}
-                      width={450}
-                    >
-                      <rect
-                        x="3"
-                        y="3"
-                        rx="10"
-                        ry="10"
-                        width="300"
-                        height="180"
-                      />
-                      <rect
-                        x="6"
-                        y="190"
-                        rx="0"
-                        ry="0"
-                        width="292"
-                        height="20"
-                      />
-                      <rect
-                        x="4"
-                        y="215"
-                        rx="0"
-                        ry="0"
-                        width="239"
-                        height="20"
-                      />
-                      <rect
-                        x="4"
-                        y="242"
-                        rx="0"
-                        ry="0"
-                        width="274"
-                        height="20"
-                      />
-                    </ContentLoader>
-                  )}
-                </div>
+                {e.images.large ? (
+                  <Image src={e.images.large} layout="fill" objectFit="fit" />
+                ) : (
+                  <ContentLoader viewBox="0 0 500 280" height={300} width={450}>
+                    <rect
+                      x="3"
+                      y="3"
+                      rx="10"
+                      ry="10"
+                      width="300"
+                      height="180"
+                    />
+                    <rect x="6" y="190" rx="0" ry="0" width="292" height="20" />
+                    <rect x="4" y="215" rx="0" ry="0" width="239" height="20" />
+                    <rect x="4" y="242" rx="0" ry="0" width="274" height="20" />
+                  </ContentLoader>
+                )}
               </section>
               <section>
                 <h3>{e.name}</h3>
