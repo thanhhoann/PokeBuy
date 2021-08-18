35d7cbdb93b14b27816658c59887610657cb1a38
thanhhoan <pdth456@gmail.com>
2021-08-18 23:55:16 +0700

    add loading state

--- components/UI/Header.js
+++ components/UI/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import pokecoinSVG from "../../public/pokecoin.svg";
