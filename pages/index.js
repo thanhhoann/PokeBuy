362b19eafc387c173634d0f9bb35f2fd24b3f0d2
thanhhoan <pdth456@gmail.com>
2021-08-19 10:03:14 +0700

    test index.js

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import { useRouter } from "next/dist/client/router";
 export default function Home() {
   const router = useRouter();
 
+  console.log("Logging in...");
   useEffect(() => {
     setTimeout(() => {
       router.push("/home");
