5525a2c06d2471704a54d7a142d53eb01dd7a1af
thanhhoan <pdth456@gmail.com>
2021-08-19 09:58:53 +0700

    remove getStatippaths

--- pages/home/explore/[id]/index.js
+++ pages/home/explore/[id]/index.js
@@ -136,24 +136,9 @@ export default function Card(props) {
   );
 }
 
-export async function getStaticPaths() {
+export async function getServerSideProps({ query }) {
   const data = await axios
-    .get(`https://api.pokemontcg.io/v2/cards/`, {
-      headers: {
-        "X-API-Key": "b53e5d64-502e-4837-948b-8d092e16fa09",
-      },
-    })
-    .then((res) => res.data.data);
-
-  const paths = data.map((e) => ({
-    params: { id: e.id },
-  }));
-  return { paths, fallback: false };
-}
-
-export async function getStaticProps({ params }) {
-  const data = await axios
-    .get(`https://api.pokemontcg.io/v2/cards/${params.id}`, {
+    .get(`https://api.pokemontcg.io/v2/cards/${query.id}`, {
       headers: {
         "X-API-Key": "b53e5d64-502e-4837-948b-8d092e16fa09",
       },
