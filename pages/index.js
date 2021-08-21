ebf72bdb08e8442f95e93b3a37a9d0a10ec6e067
thanhhoan <pdth456@gmail.com>
2021-08-21 20:47:56 +0700

    added google verify meta tag

--- pages/index.js
+++ pages/index.js
@@ -37,6 +37,10 @@ export default function Home() {
             name="description"
             content="Get your favorite pókemon cards with ease."
           />
+          <meta
+            name="google-site-verification"
+            content="Ot02JW5JSGZiiZo8tR2fMKhHxdInuawmKB1E4QG6afo"
+          />
           <link rel="icon" href="./pokecoin.svg" />
         </Head>
         <motion.section
