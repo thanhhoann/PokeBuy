095d1793e412d603786c3c146d0ee40f44279851
thanhhoan <pdth456@gmail.com>
2021-08-21 18:49:10 +0700

    add getStaticProps in contact page

--- pages/home/contact/index.js
+++ pages/home/contact/index.js
@@ -1,16 +1,31 @@
+import axios from "axios";
 import Layout from "../../../components/UI/Layout";
 import Header from "../../../components/UI/Header";
 import Hero from "../../../components/UI/Hero";
 import Footer from "../../../components/UI/Footer";
 
-export default function Contact() {
+export default function Contact(props) {
   return (
     <>
       <Layout title="Contact">
         <Header />
-        <Hero />
+        <Hero cards={props.hero} />
         <Footer />
       </Layout>
     </>
   );
+}
+
+export async function getStaticProps(context) {
+  const hero = await axios
+    .get(
+      "https://pokebuy-ecom-default-rtdb.asia-southeast1.firebasedatabase.app/hero.json"
+    )
+    .then((res) => res.data);
+
+  return {
+    props: {
+      hero,
+    },
+  };
 }
